e5e451971777a8954bb66926dceae7c54f281a2c
Michael Morrissey <mmorrissey@market76.com>
2014-09-21 14:46:49 -0400

    init commit :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Howard::Application.config.secret_key_base = '79fd23189670e756b190eb1b83be86dff39321d5f3a178281b168a842c060eacc118d86880c45af0ea1761c6b257b0c1378f7000c8c4ce65ba3e3482d2a6dede'
