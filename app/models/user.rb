72ca8337a754a5990882115c20a4ef62d5301332
Michael Morrissey <mmorrissey@market76.com>
2014-09-27 23:14:26 -0400

    sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, :name, :phone, presence: true
+  validates :email, :phone, presence: true
   validates :email, :phone, uniqueness: {:case_sensitive => false}
   validates :password, length: {within: 8..20}
+
+  before_create :set_password_confirmation
+  before_create :format_phone_number
+
+  def set_password_confirmation
+    self.password_confirmation = self.password
+  end
+
+  def format_phone_number
+    #format all phone numbers
+  end
 end
