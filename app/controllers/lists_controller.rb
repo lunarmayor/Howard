91ff0a22129c213e3b364a8f1b493515387b85d3
Michael Morrissey <mmorrissey@market76.com>
2014-10-05 22:13:55 -0400

    finish list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,36 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!
   respond_to :json
   
   def index
   	@lists = current_user.lists
-  	respond_with @lists, root: false
+  	respond_with(@lists, root: false) do |format|
+      format.html{ render 'home/index' }
+  	end
   end
 
   def show
   	@list = current_user.lists.where(id: params[:id]).includes(:notes).first
-  	respond_with @list, root: false
+  	respond_with( @list, root: false) do |format|
+      format.html { render 'home/index' }
+  	end
   end
+
+  def create
+  	list = current_user.lists.create(list_params)
+
+  	if list.save
+      respond_with list, root: false
+    end
+  end
+
+  def destroy
+  	list = List.find(params[:id])
+  	list.destroy()
+  	respond_with status: :ok
+  end
+
+  def list_params
+  	params.require(:list).permit(:name)
+  end 
 end
