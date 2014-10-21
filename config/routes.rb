03d99cdc50b05e0de35185aea5309d7e2ffa6160
Michael Morrissey <mmorrissey@market76.com>
2014-10-20 22:57:30 -0400

    better sign up / sign in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Howard::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
   root to: 'home#index'
   get '/incoming_message', to: 'message#incoming_message'
   get '/sms_all', to: 'message#sms_all'
